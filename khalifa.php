be7202c29a954368cce5c88976bf828fffc54548
Your Name <you@example.com>
2017-08-11 11:40:01 +0200

    version 1

---
+++ khalifa.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang="FR" dir="LTR">
+	<head>
+		<title>New document</title>
+		<link rel="stylesheet" type="text/css" href="khalifa.css">
+		<meta name="author" content="Khalifa EL BANAN"/>
+		<meta name="made" content="khalifa.elbanan@gmail.com"/>
+		<meta name="description" content="cette page est ..."/>
+		<meta name="keywords" content="page, html, khalifa"/>
+		<meta name="generator" content="sublime text"/>
+		<meta name="robots" content="noindex, nofollow">
+		<meta http-equiv="content type" content="text/html; charset=UTF-8"/>
+	<head>
+	<body link="red" alink="red" vlink="red">
+		<h1>Hello World</h1>
+		<table>
+		<tr>
+			<td><h3 align="center">nom</h3></td>
+			<td><h3 align="center">prenom</h3></td>
+			<td><h3 align="center">age</h3></td>
+		</tr>
+		<tr>
+			<td>EL BANAN</td>
+			<td>Khalifa</td>
+			<td>19</td>
+		</tr>
+
+		</table>
+		<?php 
+			echo "version 1";
+		?>
+	</body>
+
+</html>
